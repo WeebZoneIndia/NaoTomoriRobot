a51f2da63eaae4896d9b1604becc7d1e58e1a49c
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-05-23 12:15:58 +0530

    Small Fix

--- hitsuki/services/mongo.py
+++ hitsuki/services/mongo.py
@@ -20,7 +20,7 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from odmantic import AIOEngine
+from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
 from hitsuki import log
